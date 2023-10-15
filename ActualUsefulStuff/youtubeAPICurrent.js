92b41e83c2c2e7839f7390c47c0b36919e771a04
josephmolina256 <128499367+josephmolina256@users.noreply.github.com>
2023-10-15 02:55:39 -0400

    Updated YoutubeAPI

--- ActualUsefulStuff/youtubeAPICurrent.js
+++ ActualUsefulStuff/youtubeAPICurrent.js
@@ -1,26 +1,33 @@
-
-let vidParsing = require('./title-string-parsing');
-let vidList = vidParsing.vidList
-let API_KEY = 'AIzaSyB9PnHmwhcUg-SyCMyNxxY8CFo-rLm1oCs'
-outList = []
-for(let i = 0; i < vidList.length; i++){
-  let searchLink = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${vidList[i]}&key=${API_KEY}`
+async function youtubeFetch(vidList) {
+  for(let i = 0; i < vidList.length; i++){
+    let searchLink = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&q=${vidList[i]}&key=AIzaSyBSolnHh0xn29PRa6PspyidJBGJb2bDTRk`
   
-  fetch(searchLink)
-  .then(function (response) {
-      return response.json()
-  })
-  .then(function (obj){
-    let results = obj['items']
-    for(let j = 0; j < 3; j++){
-      outList.push({
-        'title' : items[j]["snippet"]["title"],
-        'url' : "https://www.youtube.com/watch?v="+ items[j]["id"]["videoId"],
-        'thumbnail' : items[j]["snippet"]["thumbnails"]['default']['url']
+    // const response = await fetch("http://example.com/movies.json");
+    // const movies = await response.json();
+    let response = await fetch(searchLink)
+    let temp = await response.json() 
+    let results = temp['items']
+    // console.log(results[1])
+
     
-    })
+  
+    let outList = []
+  
+    for (let j = 0; j < 3; j++) {
+      let title = results[j].snippet.title
+      let url = "https://www.youtube.com/watch?v="+ results[j]["id"]["videoId"]
+      let thumbnail = results[j]["snippet"]["thumbnails"]['default']['url']
+  
+      outList.push({ 'title' : title, 'url' : url, 'thumbnail' : thumbnail })
     }
-  })
-  .catch(error => console.log("Error"))
+  
+    await console.log(outList)
+  
+  }
+
 }
-console.log(outList)
+
+
+vidList = ['How%20to%20Design%20a%20Speed%20Gearbox']
+
+youtubeFetch(vidList)
